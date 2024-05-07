48aeb58cfe55ff178b77fc7d1b9070db17536162
durpintm <durpinthapa@gmail.com>
2024-05-06 20:21:53 -0400

    Pattern 2 and 3 completed

--- PatternProblems/Patterns.cs
+++ PatternProblems/Patterns.cs
@@ -14,5 +14,31 @@
 
             }
         }
+
+        public void Pattern2()
+        {
+            for(int i = 0;i <= 5; i++)
+            {
+                for( int j = 0;j < i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("\n");
+            }
+        }
+
+        public void Pattern3()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(j+1);
+                }
+
+                Console.WriteLine("\n");
+            }
+        }
     }
 }
--- PatternProblems/Program.cs
+++ PatternProblems/Program.cs
@@ -2,6 +2,9 @@
 
 var pattern = new Patterns();
 
-pattern.Pattern1();
+//pattern.Pattern1();
+//pattern.Pattern2();
+pattern.Pattern3();
+
 
 Console.ReadLine();
