841e149ed4254063b4526cb512201733aea0af86
durpintm <durpinthapa@gmail.com>
2024-05-06 20:16:08 -0400

    Pattern 1 Completed

---
+++ PatternProblems/Patterns.cs
@@ -0,0 +1,18 @@
+﻿namespace PatternProblems
+{
+    internal class Patterns
+    {
+        public void Pattern1()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+
+            }
+        }
+    }
+}
---
+++ PatternProblems/Program.cs
@@ -0,0 +1,7 @@
+﻿using PatternProblems;
+
+var pattern = new Patterns();
+
+pattern.Pattern1();
+
+Console.ReadLine();
