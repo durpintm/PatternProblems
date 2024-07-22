aaaed2b32065a2ab225b1053662704aa00ff6b3a
durpintm <durpinthapa@gmail.com>
2024-07-21 23:59:26 -0400

    ArrayList collections

---
+++ PatternProblems/Collections.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternProblems
+{
+    public class Collections
+    {
+        public void ArrayListEx()
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add("Hello");
+            arrayList.Add(3.14);
+        }
+
+    }
+}
